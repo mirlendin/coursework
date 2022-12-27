47034e3f58250a106aaf44107e05a3ee7d42faa2
mirlendin <118050461+mirlendin@users.noreply.github.com>
2022-12-27 22:24:26 +0200

    +

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,21 @@ let slider = $(document).ready(function () {
   });
 });
 
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const menu = document.querySelector("#menu").cloneNode(1);
+
+
+hamb.addEventListener("click", hambHandler);
+
+function hambHandler(e) {
+    e.preventDefault();
+    popup.classList.toggle("open");
+    renderPopup();
+
+}
+
+function renderPopup() {
+    popup.appendChild(menu);
+}
+
